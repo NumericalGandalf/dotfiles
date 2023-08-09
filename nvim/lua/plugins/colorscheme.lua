dc91d8a7e418a737b77ab44b0d113773cc5f8aff
BinaryGandalf <weber.jannik@hotmail.com>
2023-08-09 20:14:24 +0200

    functional colorscheme set

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,20 +1,31 @@
-require("tokyonight").setup({
-    style = "night",
-    transparent = true,
-    terminal_colors = true,
-    styles = {
-        comments = { italic = true },
-        keywords = {},
-        functions = {},
-        variables = {},
-        sidebars = "transparent",
-        floats = "transparent",
-    },
-    hide_inactive_statusline = false,
-    dim_inactive = false,
-    lualine_bold = false,
-    on_colors = function(colors) end,
-    on_highlights = function(highlights, colors) end,
-})
+vim.api.nvim_create_user_command('SetColorscheme',
+    function(opts)
+        local colorscheme = opts.fargs[1]
+        if colorscheme == "tokyonight" then
+            require("tokyonight").setup({
+                style = "night",
+                transparent = true,
+                terminal_colors = true,
+                styles = {
+                    comments = { italic = true },
+                    keywords = { italic = false },
+                    functions = {},
+                    variables = {},
+                    sidebars = "transparent",
+                    floats = "transparent",
+                },
+                hide_inactive_statusline = true,
+                dim_inactive = false,
+                lualine_bold = false,
+            })
+        else
+            error(string.format("Invalid colorscheme '%s'", colorscheme))
+            return
+        end
 
-vim.cmd [[colorscheme tokyonight]]
+        vim.api.nvim_command(string.format("colorscheme %s", colorscheme))
+    end,
+    { nargs = 1, complete = function() return { "tokyonight" } end }
+)
+
+vim.cmd.SetColorscheme("tokyonight")
