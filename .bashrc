ca0c306eef6aec752dcd4ca68ce3110fd81f3aff
BinaryGandalf <weber.jannik@hotmail.com>
2024-03-04 20:53:42 +0100

    p0

--- .bashrc
+++ .bashrc
@@ -3,3 +3,5 @@
 alias ll="ls -lah --color=auto"
 alias fd="fd -H"
 alias rg="rg --hidden -L -S -g '!.git/'"
+
+alias emacsclient="emacsclient -a 'emacs'"
--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,4 +1,29 @@
 (setq inhibit-startup-message t)
+
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
+
+(global-display-line-numbers-mode 1)
+(setq display-line-numbers-type 'relative)
+
+(set-face-attribute 'default nil :font "Hack Nerd Font Mono-11")
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melp.org/packages/"))
+
+(use-package modus-themes
+  :ensure t
+  :config
+  (load-theme 'modus-vivendi-tinted :no-confirm))
+
+(use-package evil
+  :ensure t
+  :init
+  (setq evil-want-C-u-scroll t)
+  :config
+  (evil-mode 1))
+
+(custom-set-variables
+  '(package-selected-packages '(evil modus-themes)))
+(custom-set-faces)
