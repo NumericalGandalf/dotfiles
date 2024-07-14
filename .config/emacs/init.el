cdea3098ad5c8db888696d1d5699347ce2286fdb
NumericalGandalf <weber.jannik@hotmail.com>
2024-07-14 21:44:24 +0200

    j2

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,54 +1,11 @@
-(setq custom-file (locate-user-emacs-file "var/custom.el"))
+(setq use-short-answers t
+      inhibit-startup-message t
+      custom-file (locate-user-emacs-file "var/custom.el"))
 (load custom-file :noerror :nomessage)
-
 (menu-bar-mode 0)
 (tool-bar-mode 0)
 (scroll-bar-mode 0)
-
-(blink-cursor-mode 0)
 (fringe-mode 0)
-
-(electric-pair-mode)
-(ffap-bindings)
-
-(setq display-line-numbers-type 'relative)
-(global-visual-line-mode)
-(column-number-mode)
-(global-display-line-numbers-mode)
-
-(setq use-short-answers t
-      inhibit-startup-message t)
-
-(setq backup-directory-alist `(("." . ,(locate-user-emacs-file "var/backups/")))
-      backup-by-copying t
-      version-control t
-      delete-old-versions t)
-
-(setq-default c-basic-offset 4
-              c-ts-mode-indent-offset c-basic-offset)
-
-(setq dired-listing-switches "-lah"
-        dired-free-space 'separate
-        dired-recursive-deletes 'always
-        dired-dwim-target t
-        dired-auto-revert-buffer t)
-
-(setq global-auto-revert-non-file-buffers t
-	auto-revert-remote-files t)
-(global-auto-revert-mode)
-
-(setq compilation-ask-about-save nil
-      compile-command nil)
-
-(let ((font "Fira Code"))
-  (set-face-attribute 'default nil :font font :height 130)
-  (set-face-attribute 'fixed-pitch nil :family font)
-  (set-face-attribute 'fixed-pitch-serif nil :family font)
-  (set-face-attribute 'variable-pitch nil :family font))
-
-(let ((opacity 90))
-  (set-frame-parameter nil 'alpha-background opacity)
-  (add-to-list 'default-frame-alist `(alpha-background . ,opacity)))
 
 (require 'package)
 (require 'use-package)
@@ -62,23 +19,73 @@
 
 (use-package no-littering)
 
+(with-eval-after-load 'frame
+  (let ((font "Fira Code"))
+    (set-face-attribute 'default nil :font font :height 130)
+    (set-face-attribute 'fixed-pitch nil :family font)
+    (set-face-attribute 'fixed-pitch-serif nil :family font)
+    (set-face-attribute 'variable-pitch nil :family font))
+  (blink-cursor-mode 0))
+
 (use-package doom-themes
-  :if (display-graphic-p)  
   :config
   (setq doom-themes-enable-bold t
-        doom-themes-enable-italic t
-        doom-gruvbox-dark-variant "hard")
-  (load-theme 'doom-gruvbox t))
+        doom-themes-enable-italic t)
+  (load-theme 'doom-palenight t))
+
+(use-package doom-modeline
+  :init
+  (doom-modeline-mode)
+  :config
+  (setq doom-modeline-buffer-file-name-style 'file-name-with-project))
 
 (use-package all-the-icons-dired
-  :if (display-graphic-p)
   :hook (dired-mode . all-the-icons-dired-mode)
   :config
   (setq all-the-icons-dired-monochrome nil))
 
 (use-package all-the-icons-completion
-  :if (display-graphic-p)
   :hook (marginalia-mode . all-the-icons-completion-marginalia-setup))
+
+(with-eval-after-load 'simple
+  (setq display-line-numbers-type 'relative)
+  (column-number-mode)
+  (global-display-line-numbers-mode)
+  (global-visual-line-mode)
+  (save-place-mode))
+
+(with-eval-after-load 'ffap
+  (setq ffap-require-prefix t)
+  (ffap-bindings))
+
+(with-eval-after-load 'dired
+  (setq dired-listing-switches "-lah"
+	dired-free-space 'separate
+	dired-recursive-deletes 'always
+	dired-dwim-target t
+	dired-auto-revert-buffer t)
+  (require 'dired-x))
+
+(with-eval-after-load 'files
+  (setq backup-directory-alist `(("." . ,(locate-user-emacs-file "var/backups/")))
+	backup-by-copying t
+	version-control t
+	delete-old-versions t))
+
+(with-eval-after-load 'autorevert
+  (setq global-auto-revert-non-file-buffers t
+	auto-revert-remote-files t)
+  (global-auto-revert-mode))
+
+(with-eval-after-load 'compile
+  (setq compilation-ask-about-save nil
+	compile-command nil))
+
+(with-eval-after-load 'electric
+  (setq indent-tabs-mode nil)
+  (setq-default c-basic-offset 4
+		c-ts-mode-indent-offset c-basic-offset)
+  (electric-pair-mode))
 
 (use-package orderless
   :config
@@ -98,18 +105,16 @@
 
 (use-package consult
   :bind
-  (("M-y" . consult-yank-pop)
-   ("C-s" . consult-line)
+  (([remap yank-from-kill-ring] . consult-yank-pop)
+   ([remap isearch-forward] . consult-line)
    ("C-x M-:" . consult-complex-command)
-   ("C-x b" . consult-buffer)
-   ("C-x 4 b" . consult-buffer-other-window)
-   ("C-x 5 b" . consult-buffer-other-frame)
-   ("C-x t b" . consult-buffer-other-tab)
-   ("C-x r b" . consult-bookmark)
+   ([remap switch-to-buffer] . consult-buffer)
+   ([remap switch-to-buffer-other-window] . consult-buffer-other-window)
+   ([remap switch-to-buffer-other-frame] . consult-buffer-other-frame)
+   ([remap bookmark-jump] . consult-bookmark)
    ("M-g e" . consult-compile-error)
    ("M-g f" . consult-flymake)
    ("M-g o" . consult-outline)
-   ("M-g k" . consult-global-mark)
    ("M-g i" . consult-imenu)
    ("M-p m" . consult-man)
    ("M-p f" . consult-find)
@@ -138,6 +143,7 @@
 (use-package markdown-mode)
 (use-package cmake-mode)
 (use-package yaml-mode)
+(use-package json-mode)
 
 (use-package magit)
 
@@ -146,7 +152,6 @@
   (editorconfig-mode))
 
 (use-package company
-  :if (display-graphic-p)
   :config
   (setq company-tooltip-scrollbar-width 0
         company-tooltip-idle-delay 0
@@ -173,7 +178,6 @@
   (define-key eglot-mode-map (kbd "C-c l a") 'eglot-code-actions))
 
 (use-package ligature
-  :if (display-graphic-p)
   :config
   (ligature-set-ligatures
    'prog-mode
@@ -202,6 +206,8 @@
      "{|"  "[|"  "]#"  "(*"  "}#"  "$>"  "^="))
   (global-ligature-mode))
 
+(use-package vterm)
+
 (unless (package-installed-p 'emapl)
   (package-vc-install '(emapl :url "https://github.com/NumericalGandalf/emapl.git")))
-(put 'erase-buffer 'disabled nil)
+(global-set-key (kbd "M-D") 'emapl-run)
--- README.md
+++ README.md
@@ -1,12 +1,7 @@
 # Dotfiles
 
-[Stowable](https://www.gnu.org/software/stow/) dotfiles themed with Gruvbox. Fonts are optimized for FHD displays.
+[Stowable](https://www.gnu.org/software/stow/) dotfiles. Proportions are made to fit FHD displays.
 
-### Shell
-Most env-vars are host specific afterall, therefore not part of this repo.
 
 ### Sway
-There should be a file `~/.sway/config` which includes `~/.config/sway/config` and sets host specific options like display resolutions.
-
-### Gtk
-[Gruvbox](https://github.com/Fausto-Korpsvart/Gruvbox-GTK-Theme) theme.
+There should be a file `~/.sway/config` which includes `~/.config/sway/config` and sets host specific options like the display resolution.
